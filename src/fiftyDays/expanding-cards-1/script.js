180867a6e4d91975947948acd6edebd5c1640c65
zg1993 <13122780440@163.com>
2022-08-05 10:07:16 +0800

    expanding-cards end

--- src/fiftyDays/expanding-cards-1/script.js
+++ src/fiftyDays/expanding-cards-1/script.js
@@ -1,3 +1,14 @@
-console.log('------')
-let imgList = document.querySelectorAll('.img')
-imgList.forEach(img=>{})
+
+const imgList = document.querySelectorAll('.img')
+imgList.forEach(img=>{
+    img.addEventListener('click', function(){
+        removeAllActiveClass()
+        img.classList.add('active')
+    })
+})
+
+function removeAllActiveClass(){
+    imgList.forEach(img=>{
+        img.classList.remove('active')
+    })
+}
